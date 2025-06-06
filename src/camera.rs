ba828d709f7ef34b1027c1b849580bfbebbea7e1
SherisseTJW <tanjingwensherisse@gmail.com>
2025-06-06 09:32:08 +0800

    feat: Camera struct

---
+++ src/camera.rs
@@ -0,0 +1,114 @@
+use image::{Rgb, RgbImage};
+
+use crate::{
+    objects::hittable::{Hittable, HittableList},
+    ray::{Ray, blue_gradient_vertical},
+    utils::{constants::F_INF, interval::Interval},
+    vector::{Color, Point, Vector},
+};
+
+pub struct Camera {
+    aspect_ratio: f64,
+    image_width: u32,
+    image_height: u32,
+
+    focal_length: f64,
+    viewport_width: f64,
+    viewport_height: f64,
+
+    centre: Point,
+    pixel100_loc: Point,
+    pixel_delta_u: Vector,
+    pixel_delta_v: Vector,
+}
+
+impl Camera {
+    pub fn render(&self, world: HittableList) {
+        let mut img = RgbImage::new(self.image_width, self.image_height);
+
+        for i in 0..self.image_height {
+            eprintln!(
+                "Scanning line {}.. {} remaining",
+                (i),
+                (self.image_height - i)
+            );
+
+            for j in 0..self.image_width {
+                let pixel_centre = self
+                    .pixel100_loc
+                    .addv(self.pixel_delta_u.scale(j as f64))
+                    .addv(self.pixel_delta_v.scale(i as f64));
+
+                let ray_direction = pixel_centre.addv(self.centre.negate());
+
+                let ray: Ray = Ray::new(self.centre, ray_direction);
+                let color: Color = self.ray_color(ray, &world);
+                img.put_pixel(j, i, Rgb(color.to_color()));
+            }
+        }
+
+        match img.save("image.png") {
+            Ok(_) => println!("Successfully saved"),
+            Err(e) => println!("{}", e),
+        };
+    }
+
+    pub fn ray_color(&self, ray: Ray, world: &HittableList) -> Color {
+        let world_interval: Interval = Interval::new(0.0, F_INF);
+        let hit_record = world.hit(&ray, &world_interval);
+
+        match hit_record {
+            Some(hit) => {
+                let surface_normal_vec = hit.get_normal();
+                let (x, y, z) = surface_normal_vec.get_point();
+
+                // NOTE: normalised so all x, y, and z in the range of [-1.0, 1.0]
+                // so, add 1 to move the range to [0.0, 2.0]
+                // and scale by 0.5 to get a range of [0.0, 1.0]
+                // which valid rgb values have to lie within
+                Color::new(x + 1.0, y + 1.0, z + 1.0).scale(0.5)
+            }
+            None => blue_gradient_vertical(ray),
+        }
+    }
+}
+
+impl Default for Camera {
+    fn default() -> Self {
+        let aspect_ratio = 16.0 / 9.0;
+        let image_width: u32 = 400;
+        let image_height: u32 = (image_width as f64 / aspect_ratio) as u32;
+
+        let focal_length: f64 = 1.0;
+        let viewport_height: f64 = 2.0;
+        let viewport_width: f64 = viewport_height * aspect_ratio;
+
+        let centre = Point::new(0.0, 0.0, 0.0);
+        let viewport_u: Vector = Vector::new(viewport_width, 0.0, 0.0);
+        let viewport_v: Vector = Vector::new(0.0, -viewport_height, 0.0);
+
+        let pixel_delta_u: Vector = viewport_u.scale(1.0 / image_width as f64);
+        let pixel_delta_v: Vector = viewport_v.scale(1.0 / image_height as f64);
+
+        let viewport_upper_left = centre
+            .subv(viewport_u.scale(0.5))
+            .subv(viewport_v.scale(0.5))
+            .subv(Vector::new(0.0, 0.0, focal_length));
+
+        let pixel00_loc: Point =
+            viewport_upper_left.addv(pixel_delta_u.addv(pixel_delta_v).scale(0.5));
+
+        Camera {
+            aspect_ratio,
+            image_width,
+            image_height,
+            focal_length,
+            viewport_width,
+            viewport_height,
+            centre,
+            pixel100_loc: pixel00_loc,
+            pixel_delta_u,
+            pixel_delta_v,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,14 @@
+mod camera;
 mod objects;
 mod ray;
 mod utils;
 mod vector;
 
-use image::{Rgb, RgbImage};
-
-use objects::{
-    hittable::{Hittable, HittableList},
-    sphere::Sphere,
-};
-use ray::{Ray, blue_gradient_vertical};
-use utils::{constants::F_INF, interval::Interval};
-use vector::{Color, Point, Vector};
-
-const ASPECT_RATIO: f64 = 16.0 / 9.0;
-
-const IMAGE_WIDTH: u32 = 400;
-const IMAGE_HEIGHT: u32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as u32;
-
-const VIEWPORT_HEIGHT: f64 = 2.0;
-const VIEWPORT_WIDTH: f64 = VIEWPORT_HEIGHT * ASPECT_RATIO;
-const FOCAL_LENGTH: f64 = 1.0;
+use camera::Camera;
+use objects::{hittable::HittableList, sphere::Sphere};
+use vector::Point;
 
 fn main() {
-    let camera_point: Point = Point::new(0.0, 0.0, 0.0);
-
-    let viewport_u: Vector = Vector::new(VIEWPORT_WIDTH, 0.0, 0.0);
-    let viewport_v: Vector = Vector::new(0.0, -VIEWPORT_HEIGHT, 0.0);
-
-    let delta_u: Vector = viewport_u.scale(1.0 / IMAGE_WIDTH as f64);
-    let delta_v: Vector = viewport_v.scale(1.0 / IMAGE_HEIGHT as f64);
-
-    let viewport_upper_left = camera_point
-        .subv(viewport_u.scale(0.5))
-        .subv(viewport_v.scale(0.5))
-        .subv(Vector::new(0.0, 0.0, FOCAL_LENGTH));
-
-    let pixel00_loc = viewport_upper_left.addv(delta_u.addv(delta_v).scale(0.5));
-
-    let mut img = RgbImage::new(IMAGE_WIDTH, IMAGE_HEIGHT);
-
     let sphere1: Sphere = Sphere::new(Point::new(0.0, 0.0, -1.0), 0.5);
     let sphere2: Sphere = Sphere::new(Point::new(0.0, -100.5, -1.0), 100.0);
 
@@ -47,62 +16,6 @@ fn main() {
     world.add_hittable(Box::new(sphere1));
     world.add_hittable(Box::new(sphere2));
 
-    let world_interval: Interval = Interval::new(0.0, F_INF);
-
-    for i in 0..IMAGE_HEIGHT {
-        eprintln!("Scanning line {}.. {} remaining", (i), (IMAGE_HEIGHT - i));
-
-        for j in 0..IMAGE_WIDTH {
-            let pixel_centre = pixel00_loc
-                .addv(delta_u.scale(j as f64))
-                .addv(delta_v.scale(i as f64));
-
-            let ray_direction = pixel_centre.addv(camera_point.negate());
-
-            let ray: Ray = Ray::new(camera_point, ray_direction);
-            let hit_record = world.hit(&ray, &world_interval);
-
-            match hit_record {
-                Some(hit) => {
-                    let surface_normal_vec = hit.get_normal();
-                    let (x, y, z) = surface_normal_vec.get_point();
-
-                    // NOTE: normalised so all x, y, and z in the range of [-1.0, 1.0]
-                    // so, add 1 to move the range to [0.0, 2.0]
-                    // and scale by 0.5 to get a range of [0.0, 1.0]
-                    // which valid rgb values have to lie within
-                    let color: Color = Color::new(x + 1.0, y + 1.0, z + 1.0).scale(0.5);
-                    img.put_pixel(j, i, Rgb(color.to_color()));
-                }
-                None => {
-                    let color: Color = blue_gradient_vertical(ray);
-                    img.put_pixel(j, i, Rgb(color.to_color()));
-                }
-            }
-
-            // let t = sphere.hit_at(&ray);
-            //
-            // if t > 0.0 {
-            //     let surface_vec = ray.at(t);
-            //
-            //     let surface_normal_vec = surface_vec.subv(sphere.get_centre()).unit();
-            //     let (x, y, z) = surface_normal_vec.get_point();
-            //
-            //     // NOTE: Normalised so all x, y, and z in the range of [-1.0, 1.0]
-            //     // so, add 1 to move the range to [0.0, 2.0]
-            //     // and scale by 0.5 to get a range of [0.0, 1.0]
-            //     // which valid RGB values have to lie within
-            //     let color: Color = Color::new(x + 1.0, y + 1.0, z + 1.0).scale(0.5);
-            //     img.put_pixel(j, i, Rgb(color.to_color()));
-            // } else {
-            //     let color: Color = blue_gradient_vertical(ray);
-            //     img.put_pixel(j, i, Rgb(color.to_color()));
-            // }
-        }
-    }
-
-    match img.save("image.png") {
-        Ok(_) => println!("Successfully saved"),
-        Err(e) => println!("{}", e),
-    };
+    let camera: Camera = Camera::default();
+    camera.render(world);
 }
